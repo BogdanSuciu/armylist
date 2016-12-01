fc366d93ef3cce3d6a44eb5068f3220e64195ad3
Suciu Bogdan <bogdan.suciu@gmail.com>
2016-12-01 23:31:42 +0200

    missing quick reference changes
    missing quick reference changes

--- app/modules/quick-reference/quick-reference-controller.js
+++ app/modules/quick-reference/quick-reference-controller.js
@@ -7,9 +7,15 @@
  */
 (function() {
   "use strict";
-  angular.module("armyApp").controller("quickReference", function($scope, $http) {
-    var _this = $scope;
+  angular.module("armyApp").controller("quickReference", function($scope, scopeService) {
+    if(!scopeService.shared.quickReference) {
+      scopeService.shared.quickReference = {};
+    }
+    $scope.quickReference = scopeService.shared.quickReference;
+    var _this = $scope.quickReference;
+    
     _this.ballisticTable = {1:"6",2:"5",3:"4",4:"3",5:"2",6:"2/6",7:"2/5",8:"2/4",9:"2/3",10:"2/2"};
+    
     _this.weaponSkillTable = {
       1:["4+","4+","5+","5+","5+","5+","5+","5+","5+","5+"],
       2:["3+","4+","4+","5+","5+","5+","5+","5+","5+","5+"],
@@ -22,6 +28,7 @@
       9:["3+","3+","3+","3+","3+","3+","3+","3+","4+","4+"],
       10:["3+","3+","3+","3+","3+","3+","3+","3+","3+","4+"]
     };
+    
     _this.ToWoundTable = {
       1:["4+","5+","6+","6+","-","-","-","-","-","-"],
       2:["3+","4+","5+","6+","6+","-","-","-","-","-"],
@@ -34,6 +41,10 @@
       9:["2+","2+","2+","2+","2+","2+","2+","3+","4+","5+"],
       10:["2+","2+","2+","2+","2+","2+","2+","2+","3+","4+"]
     };
-    _this.visibleTable = "ballistic";
+    
+    if(!_this.visibleTable) {
+      _this.visibleTable = "ballistic";
+    }
+    
   });
 })();
